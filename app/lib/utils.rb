1c26b9b0052b8479aa044cf5902884028ac86883
jammmin <jammmin@prakash-desktop.(none)>
2009-09-18 13:55:27 +0530

    added subsections fn

--- app/lib/utils.rb
+++ app/lib/utils.rb
@@ -10,7 +10,10 @@ module Utils
     end
     h
   end
-  
+
+  def subsections(section)
+    $layout_info[section].map{|k, v| v.class.to_s.downcase == 'hash' ? k : nil}.compact
+  end
 end
 
 # ONE TIME OP: Loads all layout info into global variable $layout_info
--- app/urls/profile.rb
+++ app/urls/profile.rb
@@ -10,7 +10,7 @@ end
 # Loads eg: jammm.in/user1/songs. 
 # :section => [songs, jams, bands]
 get '/:username/:section' do
-  sections = ['songs', 'jams', 'bands']
+  sections = subsections('profile')
   begin
     raise "Section: #{params[:section]} not found" if not sections.include?(params[:section])
     @layout_info = layout_info("profile", params[:section])
