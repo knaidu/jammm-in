ee901c41db4dc3bd1bc3478f4e582639b1764871
prakash <jammmin@prakash-desktop.(none)>
2009-10-16 14:37:49 +0530

    song page continued

--- app/urls/song.rb
+++ app/urls/song.rb
@@ -2,6 +2,7 @@
 # Loads a songs
 
 get '/song/:song_id' do
-  @layout_info = {"middle_panel" => 'song/page'}
+  @layout_info = {"middle_panel" => 'song/page', "right_panel" => 'song/right'}
+  @song = Song.find(params[:song_id])
   erb(:"body/structure")
 end
