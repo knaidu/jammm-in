1c9af4d735a491b6f64ea5e9249d19ef50274524
prakash <jammmin@prakash-desktop.(none)>
2010-02-08 16:23:44 +0530

    fixed site_counter

--- app/models/site_counter.rb
+++ app/models/site_counter.rb
@@ -3,9 +3,17 @@ class SiteCounter < ActiveRecord::Base
   set_table_name "site_counter"
   
   def self.visited(url)
+    url = self.format_url(url)
+    return if not url
     site = self.find_by_url(url) || self.create(:url => url, :counter => 0)
     site.counter += 1
     site.save
+  end
+  
+  def self.format_url(url)
+    url = url.sub("/undefined", "/")
+    url += "/" if (url.grep /\/$/).empty?
+    url
   end
   
 end
