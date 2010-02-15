f4e482715f8e0070e17bc48a4b4bc11f0e7f59f1
prakash <jammmin@prakash-desktop.(none)>
2010-02-15 16:30:08 +0530

    changes

--- app/urls/admin.rb
+++ app/urls/admin.rb
@@ -2,3 +2,18 @@ get '/admin/site_counter' do
   @layout_info = {"left_panel" => "homepage/left", "middle_panel" => "admin/site_counter"}
   erb(:'body/structure')
 end
+
+get '/admin/table/:table_name' do
+  @layout_info = {"left_panel" => "homepage/left", "middle_panel" => "admin/table"}
+  table_name = param?(:table_name).capitalize
+  @data = eval(table_name + ".all") rescue []
+  erb(:'body/structure')
+end
+
+
+get '/admin/table/:table_name/:row_id' do
+  @layout_info = {"left_panel" => "homepage/left", "middle_panel" => "admin/table"}
+  table_name = param?(:table_name).capitalize
+  @data = [eval(table_name + ".find(param?(:row_id))")] rescue []
+  erb(:'body/structure')
+end
