0228987f0d20e6027e6d6f6742b1679ba7c4b239
jammmin <jammmin@ubuntu.(none)>
2010-03-02 17:30:53 -0800

    changes

---
+++ app/restart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'Stopping Web Server...'
+sudo killall nginx
+
+echo ""
+echo "Starting Web Server..."
+sudo /opt/nginx/sbin/nginx -c /home/jammmin/webserver/app/config/nginx.conf 
+
