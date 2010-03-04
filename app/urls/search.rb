548a25e51fd2bd3cf99c2b9ba0e21e772a36b8a4
jammmin <jammmin@ubuntu.(none)>
2010-03-04 01:52:03 -0800

    changes

--- app/urls/search.rb
+++ app/urls/search.rb
@@ -2,6 +2,6 @@ get '/search' do
   key = param?(:q) or param?(:key)
   @found = search_with_key(key) rescue "[]"
   left_panel = session_user? ? "account/menu" : "homepage/menu"
-  @layout_info = {"middle_panel" => 'search/page', 'left_panel' => 'account/menu'}
+  @layout_info = {"middle_panel" => 'search/page', 'left_panel' => 'homepage/left'}
   erb(:"body/structure")
 end
