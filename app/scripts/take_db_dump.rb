45e5cb2eab773f3e6e6115b82efd8ffc3308a22a
jammmin <jammmin@ubuntu.(none)>
2010-03-11 10:12:11 -0800

    changes

--- app/scripts/take_db_dump.rb
+++ app/scripts/take_db_dump.rb
@@ -2,6 +2,6 @@ require "#{ENV["WEBSERVER_ROOT"]}/scripts/load_needed.rb"
 require 'daemons'
 
 cron_log_new_section("TAKING AT DATABASE DUMP")
-#Daemons.daemonize # Runs the script in the background
+Daemons.daemonize # Runs the script in the background
 
 Dump.take_dump
