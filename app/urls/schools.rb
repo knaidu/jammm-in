fdff43593587bcf4a34a02b895992f40a4810e54
jammmin <jammmin@ubuntu.(none)>
2010-08-04 19:23:37 -0700

    changes

--- app/urls/schools.rb
+++ app/urls/schools.rb
@@ -1,16 +1,22 @@
-get '/schools/:name' do
+get '/schools/:handle' do
   school_exists?{
     @layout_info = {"middle_panel" => 'schools/middle', "left_panel" => "schools/left", "right_panel" => "schools/right"}
     erb(:"body/structure")
   }
 end
 
-get '/schools/:name/jams' do
+get '/schools/:handle/jams' do
   @layout_info = {"middle_panel" => 'schools/jams', "left_panel" => "schools/left", "right_panel" => "schools/right"}
   erb(:"body/structure")
 end
 
-get '/schools/:name/songs' do
+get '/schools/:handle/songs' do
   @layout_info = {"middle_panel" => 'schools/songs', "left_panel" => "schools/left", "right_panel" => "schools/right"}
   erb(:"body/structure")
 end
+
+get '/schools/:handle/admin' do
+  @layout_info = {"middle_panel" => 'schools/admin', "left_panel" => "schools/left", "right_panel" => "schools/right"}
+  erb(:"body/structure")
+end
+
