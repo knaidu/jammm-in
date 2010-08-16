155fa6aff8bd8092ca3bbc451bf5c9e2304aa6d9
jammmin <jammmin@jammmin.(none)>
2010-08-16 13:53:26 -0400

    Added Admin pages for School Admin

--- app/urls/schools.rb
+++ app/urls/schools.rb
@@ -25,8 +25,39 @@ end
 get '/schools/:handle/admin' do
   school_exists?{
     @school = get_passed_school
-    @layout_info = {"middle_panel" => 'schools/admin/middle', "left_panel" => "schools/left", "right_panel" => "schools/right"}
+    @layout_info = {"middle_panel" => 'schools/admin/middle', "left_panel" => "schools/admin/left", "right_panel" => "schools/admin/right"}
     erb(:"body/structure")
   }
 end
 
+get '/schools/:handle/admin/add' do
+  school_exists?{
+    @school = get_passed_school
+    @layout_info = {"middle_panel" => 'schools/admin/add', "left_panel" => "schools/admin/left", "right_panel" => "schools/admin/right"}
+    erb(:"body/structure")
+  }
+end
+
+get '/schools/:handle/admin/delete' do
+  school_exists?{
+    @school = get_passed_school
+    @layout_info = {"middle_panel" => 'schools/admin/delete', "left_panel" => "schools/admin/left", "right_panel" => "schools/admin/right"}
+    erb(:"body/structure")
+  }
+end
+
+get '/schools/:handle/admin/manage' do
+  school_exists?{
+    @school = get_passed_school
+    @layout_info = {"middle_panel" => 'schools/admin/manage', "left_panel" => "schools/admin/left", "right_panel" => "schools/admin/right"}
+    erb(:"body/structure")
+  }
+end
+
+get '/schools/:handle/admin/list' do
+  school_exists?{
+    @school = get_passed_school
+    @layout_info = {"middle_panel" => 'schools/admin/list', "left_panel" => "schools/admin/left", "right_panel" => "schools/admin/right"}
+    erb(:"body/structure")
+  }
+end
