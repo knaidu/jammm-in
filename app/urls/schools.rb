94daf7ab40bf32ddee47bffbee363d00ea0f951f
jammmin <jammmin@ubuntu.(none)>
2010-08-22 08:30:28 -0700

    merging of add users and list users

--- app/urls/schools.rb
+++ app/urls/schools.rb
@@ -38,22 +38,11 @@ get '/schools/:handle/admin' do
   }
 end
 
-get '/schools/:handle/admin/add' do
+get '/schools/:handle/admin/add_users' do
   school_exists?{
     @school = get_passed_school
-    @layout_info = {"middle_panel" => 'schools/admin/add', "left_panel" => "schools/admin/left", "right_panel" => "schools/admin/right"}
+    @layout_info = {"middle_panel" => 'schools/admin/add_users', "left_panel" => "schools/admin/left", "right_panel" => "schools/admin/right"}
     erb(:"body/structure")
-  }
-end
-
-post '/schools/:handle/admin/add_user' do
-  monitor{
-    @school = get_passed_school
-    raise "School not found. Please check the school been passed" if @school.nil?
-    user = User.with_username(param?(:username))
-    raise "User with username '#{param?(:username)}' is not found. Please check the username" if user == nil
-    @school.add_user(user)
-    "The user has been successfully added to the school"
   }
 end
 
@@ -73,36 +62,22 @@ get '/schools/:handle/admin/manage' do
   }
 end
 
-get '/schools/:handle/admin/list' do
+get '/schools/:handle/admin/list_users' do
   school_exists?{
     @school = get_passed_school
-    @layout_info = {"middle_panel" => 'schools/admin/list', "left_panel" => "schools/admin/left", "right_panel" => "schools/admin/right"}
+    @layout_info = {"middle_panel" => 'schools/admin/list_users', "left_panel" => "schools/admin/left", "right_panel" => "schools/admin/right"}
     erb(:"body/structure")
   }
 end
 
-post '/schools/:handle/admin/add_update' do # you had mentioned this api with the "get" method, but your form was using a "post" method. all operations must be post
+post '/schools/:handle/admin/add_user_api' do # you had mentioned this api with the "get" method, but your form was using a "post" method. all operations must be post
   monitor {
     username = params[:name] # variable name does not follow camel casing, hence it should be username or user_name.
-    school = School.with_handle(params[:handle])
+    school = get_passed_school
     raise "The specifed school does not exists" unless school
     user =  User.with_username(username)
-    raise "The specifed artist does not exist." unless user # "unless" is equivalent to "if not"
+    raise "The specifed artist does not exist." if user.nil? # "unless" is equivalent to "if not"
     ret = school.add_user(user)
     "Successfully Added user to the School"
   } # Look at what "monitor" does. in helpers/all.rb
 end
-
-
-post '/schools/:handle/admin/delete_user' do # you had mentioned this api with the "get" method, but your form was using a "post" method. all operations must be post
-  monitor {
-    username = params[:name] # variable name does not follow camel casing, hence it should be username or user_name.
-    school = School.with_handle(params[:handle])
-    raise "The specifed school does not exists" unless school
-    user =  User.with_username(username)
-    raise "The specifed artist does not exist." unless user # "unless" is equivalent to "if not"
-    ret = school.add_user(user)
-    "Successfully Added user to the School"
-  } # Look at what "monitor" does. in helpers/all.rb
-end
-
